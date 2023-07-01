037c453f33bf3bb4b102186f70001bd33989d752
Tomas Pereira <tomaspereira2k@gmail.com>
2023-07-01 06:40:24 -0300

    challenge finish

--- modal.js
+++ modal.js
@@ -1,12 +1,18 @@
 const requireEmailMessage = document.querySelector(".required-email-message");
+const newsletterComponentContainer = document.querySelector(".newsletter-component-container");
+const modalContainer = document.querySelector(".form-success-container");
+const modalCloseBtn = document.querySelector(".modal-close-btn");
+const formSubmit = document.querySelector("form");
+const formSuccessTextSpan = document.querySelector(".form-success-text-span");
 const emailInput = document.querySelector(".input");
 
 
 const Regex = /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
 
+let email = null;
+
 const updateEmail = ()=>{
-    let email = emailInput.value;
-    console.log(email, Regex.test(email))
+    email = emailInput.value;
     if(!Regex.test(email)){
         requireEmailMessage.style.opacity = "100%";
         emailInput.style.backgroundColor = " hsl(5, 100%, 95%)";
@@ -21,3 +27,19 @@ const updateEmail = ()=>{
     }
 }
 emailInput.addEventListener("input", updateEmail);
+
+const showModal = (e)=>{
+        e.preventDefault()
+        modalContainer.classList.toggle("open-modal")
+        modalContainer.style.display = "flex";
+        newsletterComponentContainer.style.display = "none";
+        formSuccessTextSpan.innerText = `${email}`
+}
+formSubmit.addEventListener("submit", showModal);
+
+const closeModal = ()=>{
+        modalContainer.style.display = "none";
+        newsletterComponentContainer.style.display = "flex";
+        modalContainer.classList.toggle("open-modal");
+}
+modalCloseBtn.addEventListener("click", closeModal);
